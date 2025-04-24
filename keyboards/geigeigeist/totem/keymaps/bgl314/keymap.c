e221da164293b33aa66f58180e874a52fca147ed
Bgl <bgl314@yahoo.com>
2025-04-23 22:06:11 -0400

    Synced with ZMK gaming

--- keyboards/geigeigeist/totem/keymaps/bgl314/keymap.c
+++ keyboards/geigeigeist/totem/keymaps/bgl314/keymap.c
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 KC_Q,    KC_W,      KC_F,      KC_P,    KC_G,    KC_J,      KC_L,      KC_U,     KC_Y,    KC_QUOT,
                 KC_A,    ALT_R,     CTL_S,     SHT_T,   KC_D,    KC_H,      SHT_N,     CTL_E,    ALT_I,   KC_O,
         KC_Z,   KC_Z,    KC_X,      KC_C,      KC_V,    KC_B,    KC_K,      KC_M,      KC_COMM,  KC_DOT,  KC_SLSH, KC_BSLS,
-    MT(MOD_LGUI,KC_ESC),LT(_NUMBERS, KC_TAB),MT(MOD_LCTL,KC_BSPC), LT(_NAV,KC_SPC),OSM(MOD_RSFT) , MO(_SYMBOLS)
+    MT(KC_LALT,KC_ESC),LT(_NUMBERS, KC_TAB),MT(MOD_LCTL,KC_BSPC), LT(_NAV,KC_SPC),OSM(MOD_RSFT) , MO(_SYMBOLS)
     ),
     [_MOUSE] = LAYOUT(
  //╷         ╷         ╷         ╷         ╷         ╷         ╷╷         ╷         ╷         ╷         ╷         ╷         ╷
@@ -66,17 +66,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,  _______,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,   _______, _______,
                             MO(_GAMES_ALT),KC_TAB,    KC_SPC,       _______, _______, KC_RGUI
     ),
+    [_GAMES_QWE] = LAYOUT(
+      //╷         ╷         ╷         ╷         ╷         ╷         ╷╷         ╷         ╷         ╷         ╷         ╷         ╷
+            KC_Q,     KC_W,      KC_E,    KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,
+            KC_A,     KC_S,      KC_D,    KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,
+   _______, KC_Z,    KC_X,       KC_C,    KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,   _______, 
+                           MO(_GAMES_ALT),KC_TAB, KC_SPC,       _______, _______, KC_RGUI
+         ),
     [_GAMES_ALT] = LAYOUT(
  //╷         ╷         ╷         ╷         ╷         ╷         ╷╷         ╷         ╷         ╷         ╷         ╷         ╷
-                KC_F9,    KC_F10,  KC_F11,   KC_F12,  KC_NO,      _______,  _______,  _______,  _______,  _______,
-                KC_F5,    KC_F6,   KC_F7,   KC_F8,  KC_BSPC,      KC_H,     KC_N,     KC_E,     KC_I,     KC_O,
-    _______,    KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_ESC,      _______,  _______,  _______,  _______,  _______,  _______,
+                KC_ESC,   KC_F7,   KC_F8,  KC_F9,   KC_F10,       _______,  _______,  _______,  _______,  _______,
+                KC_BSPC,  KC_F4,    KC_F5,   KC_F6,   KC_F11,       _______,  _______,  _______,  _______,  _______,
+    _______,    KC_DEL,   KC_F1,    KC_F2,   KC_F3,   KC_F12,      _______,  _______,  _______,  _______,  _______,  _______,
                                     _______, _______, _______,   _______,  _______, _______
     ),
     [_SYMBOLS] = LAYOUT(
  //╷         ╷         ╷         ╷         ╷         ╷         ╷╷         ╷         ╷         ╷         ╷         ╷         ╷
                 KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,     KC_CIRC,   KC_AMPR, KC_ASTR,   KC_LPRN, KC_RPRN,
-
                 KC_TILDE, KC_GRV, KC_PIPE, KC_PLUS,  KC_EQL,    KC_LT,   KC_LPRN,  KC_LCBR,KC_LBRC, KC_GT,
     _______,    KC_BSLS,  KC_NO, KC_UNDS, KC_MINUS,KC_NO ,          KC_NO,   KC_RPRN, KC_RCBR ,KC_RBRC, KC_BSLS,_______,
                                   KC_LALT,KC_LSFT,  _______,     _______,  _______,  _______
@@ -98,7 +104,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #ifdef HAS_PASSWORDS
     [_ADJUST] = LAYOUT(
  //╷         ╷         ╷         ╷         ╷         ╷         ╷╷         ╷         ╷         ╷         ╷         ╷         ╷
-                KC_MNXT , KC_F7, KC_F8, KC_F9,   KC_F10,      KC_NO, KC_NO,   KC_NO,    KC_NO, TG(_GAMES),
+                KC_MNXT , KC_F7, KC_F8, KC_F9,   KC_F10,      KC_NO, KC_NO,   TO(_COLEMAK),    TG(_GAMES_QWE), TG(_GAMES),
                 KC_MPLY,KC_F4,KC_F5, KC_F6,       KC_F11,         OSL(_PWDS),  OSM(MOD_RSFT), OSM(MOD_RCTL),  OSM(MOD_RALT),  OSM(MOD_RGUI),
     _______,    KC_MSTP, KC_F1,   KC_F2,   KC_F3,   KC_F12,        KC_NO,KC_VOLU, KC_VOLD, KC_MUTE,   OSL(_PWDS),OSL(_PWDS),
                                  KC_LALT,  KC_LSFT,  KC_LCTL, _______,   _______,   _______
@@ -113,9 +119,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #else
 [_ADJUST] = LAYOUT(
  //╷         ╷         ╷         ╷         ╷         ╷         ╷╷         ╷         ╷         ╷         ╷         ╷         ╷
-                KC_MNXT , KC_F7, KC_F8, KC_F9,   KC_F10,      KC_NO, KC_NO,   KC_NO,    KC_NO, TG(_GAMES),
-                KC_MPLY,KC_F4,KC_F5, KC_F6,       KC_F11,         KC_NO,  OSM(MOD_RSFT), OSM(MOD_RCTL),  OSM(MOD_RALT),  OSM(MOD_RGUI),
-    _______,    KC_MSTP, KC_F1,   KC_F2,   KC_F3,   KC_F12,        KC_NO,KC_VOLU, KC_VOLD, KC_MUTE,   KC_NO,_______,
+                KC_MNXT , KC_F7, KC_F8, KC_F9,   KC_F10,         KC_NO, KC_NO,   TG(_COLEMAK), TG(_GAMES_QWE), TG(_GAMES),
+                KC_MPLY,  KC_F4, KC_F5, KC_F6,   KC_F11,         KC_NO,  OSM(MOD_RSFT), OSM(MOD_RCTL),  OSM(MOD_RALT),  OSM(MOD_RGUI),
+    _______,    KC_MSTP, KC_F1,   KC_F2,   KC_F3,   KC_F12,      KC_NO,KC_VOLU, KC_VOLD, KC_MUTE,   KC_NO,_______,
                                 KC_LALT,  KC_LSFT,  KC_LCTL, _______,   _______,   _______
     )
 #endif
